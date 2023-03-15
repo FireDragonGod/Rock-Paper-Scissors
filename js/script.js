65ccae5b5bb2b5473c47d164c800d4368ebedb4a
Murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-03-16 07:05:20 +0800

    Display players and running score and result

--- js/script.js
+++ js/script.js
@@ -28,4 +28,44 @@ const computerChoice = function getComputerChoice(choices) {
 };
 
 
+let playerScore = 0;
+let computerScore = 0;
 
+const playRound = function setTheGameRules(playerSelection, computerSelection) {
+  let result = document.querySelector('.result');
+
+  const playerScoreDoc = document.querySelector('.player-score');
+  const computerScoreDoc = document.querySelector('.computer-score');
+
+  if (playerSelection === computerSelection) {
+    result.textContent = 'It\'s a tie';
+  } else if (playerSelection === 'rock')
+    if (computerSelection === 'scissors') {
+      result.textContent = 'You win! Rock destroys scissors.';
+      playerScore += 1;
+      playerScoreDoc.textContent = `${playerScore}`;
+    } else {
+      result.textContent = 'Computer wins! Paper wraps rock';
+      computerScore += 1;
+      computerScoreDoc.textContent = `${computerScore}`;
+  } else if (playerSelection === 'paper')
+    if (computerSelection === 'rock') {
+      result.textContent = 'You win! paper wraps rock';
+      playerScore += 1;
+      playerScoreDoc.textContent = `${playerScore}`;
+    } else {
+      result.textContent = 'Computer wins! Scissors cuts paper';
+      computerScore += 1;
+      computerScoreDoc.textContent = `${computerScore}`;
+  } else if (playerSelection === 'scissors')
+    if (computerSelection === 'paper') {
+      result.textContent = 'You win! Scissors cuts paper';
+      playerScore += 1;
+      playerScoreDoc.textContent = `${playerScore}`;
+    } else {
+      result.textContent = 'Computer wins! Rock destroys scissors!';
+      computerScore += 1;
+      computerScoreDoc.textContent = `${computerScore}`;
+  }
+  return result;
+};
